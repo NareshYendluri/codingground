4a4a06d284e9204b04c0058d35dec6385c911f34
NareshYendluri <naresh.yendluri@gmail.com>
2017-04-25 17:22:31 +0530

    Created by Coding Ground Client

---
+++ resourceService/resourceService.js
@@ -0,0 +1,31 @@
+"use strict";
+angular.module("myApp", ['ngResource']);
+angular.module("myApp").factory('User', function($http){
+    var url = "https://jsonplaceholder.typicode.com/posts";
+    return $resource(url, {id:'@id'});
+});
+angular.module("myApp").controller("mainCtrl",['User', function(User){
+    var vm =this;
+    vm.users = [];
+    vm.selectedUser = null;
+    
+    vm.userId = "";
+    vm.title = "";
+    vm.body = "";
+    
+    vm.getUsers = function(){
+       vm.users = User.query();
+    };
+    vm.getUser = function(id){
+       vm.users = User.get({id:id});
+    };
+    vm.deleteUser = function(id){
+       vm.users = User.delete({id:id});
+    };
+    vm.addUser = function(){
+        var usr = new User({userId: vm.userId, title: vm.title, body:vm.body});
+       usr.$save(function(newUser, responseHeader){
+           vm.selectedUser = newUser;
+       })
+    };
+}]);
