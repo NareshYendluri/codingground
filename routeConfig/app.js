7cf61af2e5e636b79a2cd6d6c3e6ef8ccf00deb3
NareshYendluri <naresh.yendluri@gmail.com>
2017-04-26 11:57:32 +0530

    Created by Coding Ground Client

---
+++ routeConfig/app.js
@@ -0,0 +1,29 @@
+"use strict";
+angular.module("myApp", ['ngRoute']);
+angular.module("myApp").config(['$routeProvider', function($routeProvider){
+    $routeProvider
+    .when("/welcome", {
+      templateUrl: "views/welocme.html",
+      controller: "WelcomeCtrl",
+      controllerAs: "vm"
+    })
+    .when("/about", {
+        templateUrl: "views/about.html",
+      controller: "AboutCtrl",
+      controllerAs: "vm"
+    })
+    .when("/contact", {
+        templateUrl: "views/contact.html",
+      controller: "AboutCtrl",
+      controllerAs: "vm"
+    })
+}]);
+angular.module("myApp").controller("AboutCtrl",[function(){
+    this.details = "Hello.. I am from About Controller";
+}]);
+angular.module("myApp").controller("ContactCtrl",[function(){
+    this.details = "Hello.. I am from Contact Controller";
+}]);
+angular.module("myApp").controller("WelcomeCtrl",[function(){
+    this.details = "Hello.. I am from Welcome Controller";
+}]);
