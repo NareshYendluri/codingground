6ec692125453c93505b9673e64dd02fc0120908c
NareshYendluri <naresh.yendluri@gmail.com>
2017-04-26 20:25:46 +0530

    Created by Coding Ground Client

---
+++ customePropsOnRoute/libs/app.js
@@ -0,0 +1,39 @@
+"use strict";
+angular.module("myApp", ['ngRoute']);
+angular.module("myApp").config(['$routeProvider', function($routeProvider){
+    $routeProvider
+    .when("/welcome", {
+        templateUrl:"views/welcome.html",
+        controller:"WelcomeCtrl",
+        controllerAs: "vm",
+        data : {
+            customProp1: "Custome Value#1",
+            customProp2: "Custome Value#2"
+        }
+    })
+    .when("/about", {
+        templateUrl:"views/about.html",
+        controller:"AboutCtrl",
+        controllerAs: "vm",
+        data : {
+            customProp3: "Custome Value#3",
+            customProp4: "Custome Value#4"
+        }
+    })
+    .otherwise({
+        redirectTo:"/welcome"
+    })
+}]);
+angular.module("myApp").controller("WelcomeCtrl",['$route', function($route){
+    this.details = "Hello, from WelcomeController";
+    this.data = $route.current.data;
+}]);
+angular.module("myApp").controller("AboutCtrl",['$route', function($route){
+    this.details = "Hello, from AboutController";
+    this.data = $route.current.data;
+}]);
+
+
+
+
+
